5558913ff46376c7cb3f5148626796b1aca5f4ba
cryoleet <vishalpremnath09@gmail.com>
2024-03-10 21:08:15 +0530

    made changes in form validation

--- search.js
+++ search.js
@@ -3,15 +3,37 @@ var app = angular.module("search", []);
 app.controller("find", function($scope, $http) {
 
 $scope.articles = [];
+$scope.sortby = "relevancy";
+
+$scope.$watchGroup(['fromDate', 'toDate'], function(newValues) {
+    var fromDate = new Date(newValues[0]);
+    var toDate = new Date(newValues[1]);
+
+
+    if (toDate < fromDate) {
+        $scope.searchForm.toDate.$setValidity('beforeDate', false);
+    } 
+    else {
+        $scope.searchForm.toDate.$setValidity('beforeDate', true);
+    }
+
+  });
 
 $scope.go = function() {
 
     let keyword = $scope.keyword;
-    let from = $scope.fromDate.toISOString().split('T')[0];
-    let to = $scope.toDate.toISOString().split('T')[0];
+    let from = "";
+    if ($scope.fromDate)
+    {
+        from = "&from=" + $scope.fromDate.toISOString().split('T')[0];
+    }
+    let to = "";
+    if ($scope.toDate)
+    {
+        to = "$to=" + $scope.toDate.toISOString().split('T')[0];
+    }
     let sort = $scope.sortby;
-
-    var url = `https://newsapi.org/v2/everything?q=${keyword}&from=${from}&to=${to}&sortBy=${sort}&apiKey=84e9019ac272472cab269d40cae1b6f4`;
+    var url = `https://newsapi.org/v2/everything?q=${keyword}${from}${to}&sortBy=${sort}&apiKey=84e9019ac272472cab269d40cae1b6f4`;
     console.log("API URL:", url); // Debug statement to check the API URL
     $http.get(url)
     .then(function(response) {
